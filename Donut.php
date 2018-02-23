fdbeded884b8e2fe2b3ba2b764ec43576b66873f
sirgalas <hudos@rambler.ru>
2018-02-23 08:55:14 +0300

    redact now

--- Donut.php
+++ Donut.php
@@ -1,23 +1,13 @@
-<?php
-
-namespace sirgalas\Morris;
-
-use yii\helpers\ArrayHelper;
-
-class Donut extends Base
-{
-    public $donutColor;
-
+ public $donutColor;
+    public $formatter;
     protected $type = 'Donut';
 
     protected function getViewOptions()
     {
         $viewOptions = parent::getViewOptions();
-
         $viewOptions = ArrayHelper::merge($viewOptions, [
             'donutColor' => $this->donutColor,
+            'formatter'   => $this->formatter
         ]);
-
         return $viewOptions;
     }
-}
